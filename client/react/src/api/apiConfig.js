ed93153794c243295674f6cee3db0d21f60b0c5e
Seth Reid <contact@sethreid.co.nz>
2017-05-25 09:31:58 +1200

    testing .nv

--- client/react/src/api/apiConfig.js
+++ client/react/src/api/apiConfig.js
@@ -1,10 +1,7 @@
-let ApiConfig = {}
-
-if (process.env.NODE_ENV === 'production') {
-    ApiConfig.apiHost = window.location.origin
+console.log(process.env)
+let ApiConfig = {
+    host: process.env.API_HOST ? process.env.API_HOST : window.location.origin
 }
-else{
-    ApiConfig.apiHost =  'http://localhost:5000'
-}
+console.log(ApiConfig)
 
 export default ApiConfig
